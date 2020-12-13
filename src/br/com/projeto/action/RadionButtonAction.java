b13aac2b46b9dfc8bae726036b722703e4372225
mlima3696 <mjuniorlima3@gmail.com>
2020-12-13 20:14:05 -0300

    StrutsAula30

--- src/br/com/projeto/action/RadionButtonAction.java
+++ src/br/com/projeto/action/RadionButtonAction.java
@@ -1,16 +1,51 @@
 package br.com.projeto.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.opensymphony.xwork2.ActionSupport;
 
+import br.com.projeto.model.Language;
+
 public class RadionButtonAction extends ActionSupport {
-	
-	private List<String> gender;
-	
+
+	private List<String> genders;
+	private List<Language> languages;
+
+	private String youGender;
+	private String youLanguage;
+	private String youAnswer;
+
+	private static final String Male = "M";
+	private static final String Female = "F";
+	private static final String UNKNOWN = "Inderteminado";
+
+	public RadionButtonAction() {
+		genders = new ArrayList<String>();
+		genders.add(Male);
+		genders.add(Female);
+		genders.add(UNKNOWN);
+
+		languages = new ArrayList<Language>();
+		languages.add(new Language("ES", "Espanha"));
+		languages.add(new Language("Us", "EUA"));
+		languages.add(new Language("BR", "Brasil"));
+		languages.add(new Language("CH", "China"));
+
+	}
+
+	public String getDefaultLanguageValue() {
+		return "BR";
+	}
+
+	public String getDefaultGenderValue() {
+		return UNKNOWN;
+	}
+
 	public String display() {
 		return NONE;
 	}
+
 	public String execute() {
 		System.out.println("Executou Radio Button");
 		return "sucsess";
--- src/br/com/projeto/model/Language.java
+++ src/br/com/projeto/model/Language.java
@@ -1,5 +1,12 @@
 package br.com.projeto.model;
 
 public class Language {
+	private String LanguageCode;
+	private String LanguageDisplay;
+
+	public Language(String languageCode, String languageDisplay) {
+		LanguageCode = languageCode;
+		LanguageDisplay = languageDisplay;
+	}
 
 }
